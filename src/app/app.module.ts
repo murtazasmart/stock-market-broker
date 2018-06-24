3f7eaeadc76b6ecfdf0ee3b1baee8c81de29a78a
Bhagya Rathnayake <bhagya@omobio.net>
2018-06-24 14:47:40 +0530

    CPU module conflicts resolved.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,37 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import {AppRoutingModule} from './app.routing';
-import {NavbarModule} from './views/shared/navbar/navbar.module';
-import {FooterModule} from './views/shared/footer/footer.module';
-import {SidebarModule} from './views/sidebar/sidebar.module';
-import {LbdModule} from './lbd/lbd.module';
+import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './views/shared/navbar/navbar.module';
+import { FooterModule } from './views/shared/footer/footer.module';
+import { SidebarModule } from './views/sidebar/sidebar.module';
+import { LbdModule } from './lbd/lbd.module';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
-import {HomeComponent} from './views/home/home.component';
-import {UserComponent} from './views/user/user.component';
-import {TablesComponent} from './views/tables/tables.component';
-import {TypographyComponent} from './views/typography/typography.component';
-import {IconsComponent} from './icons/icons.component';
-import {NotificationsComponent} from './views/notifications/notifications.component';
-import {UpgradeComponent} from './views/upgrade/upgrade.component';
-import {JoinComponent} from './views/join/join.component';
-import {SharepriceComponent} from './views/shareprice/shareprice.component';
-import {JoinServiceService} from './services/join-service.service';
-import {JoinedDetailServiceService} from './services/joined-detail-service.service';
-import {AnalystServiceService} from './services/analyst-service.service';
-import {TransactionsServiceService} from './services/transactions-service.service';
-import {BrokerServiceService} from './services/broker-service.service';
-import {SimulatorServiceService} from './services/simulator-service.service'
-import {GameServiceService} from './services/game-service.service'
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import { HomeComponent } from './views/home/home.component';
+import { UserComponent } from './views/user/user.component';
+import { TablesComponent } from './views/tables/tables.component';
+import { TypographyComponent } from './views/typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { NotificationsComponent } from './views/notifications/notifications.component';
+import { UpgradeComponent } from './views/upgrade/upgrade.component';
+import { JoinComponent } from './views/join/join.component';
+import { SharepriceComponent } from './views/shareprice/shareprice.component';
+import { JoinServiceService } from './services/join-service.service';
+import { JoinedDetailServiceService } from './services/joined-detail-service.service';
+import { AnalystServiceService } from './services/analyst-service.service';
+import { TransactionsServiceService } from './services/transactions-service.service';
+import { BrokerServiceService } from './services/broker-service.service';
+import { SimulatorServiceService } from './services/simulator-service.service'
+import { GameServiceService } from './services/game-service.service'
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { SpinnerComponent } from './views/spinner/spinner.component';
+import { CpuComponent } from './cpu/cpu.component'
+import { CpuPlayerService } from './services/cpu-player.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,7 +44,8 @@ import { SpinnerComponent } from './views/spinner/spinner.component';
     UpgradeComponent,
     JoinComponent,
     SharepriceComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    CpuComponent
 
   ],
   imports: [
@@ -57,16 +60,17 @@ import { SpinnerComponent } from './views/spinner/spinner.component';
     LbdModule
   ],
   providers: [
-    JoinServiceService, 
+    JoinServiceService,
     JoinedDetailServiceService,
     AnalystServiceService,
     TransactionsServiceService,
     BrokerServiceService,
     SimulatorServiceService,
     GameServiceService,
-    {provide:LocationStrategy,useClass:HashLocationStrategy}
-    
+    CpuPlayerService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
